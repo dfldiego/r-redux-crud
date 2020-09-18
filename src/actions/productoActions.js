75cbf29ecc44b821907b16bfcaf79c4b94be1490
dfldiego <dfl.diego92@gmail.com>
2020-09-18 18:57:26 -0300

    Obtener los Productos de la API. parte3. obteniendo los productos en el state. realizando action de producto exitoso y de error.

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -65,13 +65,31 @@ const agregarProductoError = estado => ({
 export function obtenerProductosAction() {
     return async (dispatch) => {
         dispatch(descargarProductos());
+        try {
+            const respuesta = await clienteAxios.get('/productos');
+            /* console.log(respuesta.data); */
+            dispatch(descargarProductosExitosa(respuesta.data));
+        } catch (error) {
+            console.log(error);
+            dispatch(descargarProductosError());
+        }
     }
 }
 
 const descargarProductos = () => ({
     type: COMENZAR_DESCARGA_PRODUCTOS,
     payload: true
-})
+});
+
+const descargarProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+});
+
+const descargarProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
+});
 
 /**
  * productosReducer y productoActions tienen los mismos types.
