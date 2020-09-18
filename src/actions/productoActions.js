20adaac74c13c7a86929867449e5d35ab4c24edc
dfldiego <dfl.diego92@gmail.com>
2020-09-18 12:15:36 -0300

    definiendo las primeras acciones de Redux. creamos los primeros types. creamos archivo de actions.

---
+++ src/actions/productoActions.js
@@ -0,0 +1,15 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR,
+} from '../types';
+
+
+
+
+/**
+ * productosReducer y productoActions tienen los mismos types.
+ * aqui tenemos una funcion que se va a utilizar en la vista.
+ * por ejemplo: creamos un nuevo objeto de producto, esa funcion hay que usarla
+ * se le pasa un valor y se consume en esta parte.
+ */
---
+++ src/types/index.js
@@ -0,0 +1,8 @@
+export const AGREGAR_PRODUCTO = 'AGREGAR_PRODUCTO';
+export const AGREGAR_PRODUCTO_EXITO = 'AGREGAR_PRODUCTO_EXITO';
+export const AGREGAR_PRODUCTO_ERROR = 'AGREGAR_PRODUCTO_ERROR';
+
+/**
+ * se usan para describir que pasa en nuestra aplicacion
+ * se usan en el action para ir ejecutando ciertas funciones y en el reducer
+ */
