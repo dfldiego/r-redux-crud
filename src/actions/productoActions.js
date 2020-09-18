7b583cdd322a94c5cacb052c2fa05cec1f46605a
dfldiego <dfl.diego92@gmail.com>
2020-09-18 13:48:05 -0300

    Creación de Productos y Validación. parte1. agregando un Nuevo Producto al state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,12 +6,36 @@ import {
 
 // crear nuevos productos
 export function crearNuevoProductoAction(producto) {
-    return () => {
+    return (dispatch) => {
         // aqui es donde se inserta en la BBDD y se manda a ejecutar el reducer para modificar el state
         // esta funcion la vamos a usar en el componente(vista)
-        console.log(producto);
+        /* console.log(producto); */
+        dispatch(agregarProducto());
+
+        // hacemos consulta a la BBDD
+        try {
+            // si todo sale bien
+            dispatch(agregarProductoExito(producto));
+        } catch (error) {
+            // si hay un error
+            dispatch(agregarProductoError(true));
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+})
+
+// si el producto se guarda en la BBDD
+const agregarProductoExito = producto => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+});
+
+// si hubo un error
+const agregarProductoError = () => { }
 
 
 /**
@@ -19,4 +43,7 @@ export function crearNuevoProductoAction(producto) {
  * aqui tenemos una funcion que se va a utilizar en la vista.
  * por ejemplo: creamos un nuevo objeto de producto, esa funcion hay que usarla
  * se le pasa un valor y se consume en esta parte.
+ * payload: lo que va a modificar el state
+ * action.type -> describe la aplicacion
+ * action.payload -> modificar la aplicacion
  */
